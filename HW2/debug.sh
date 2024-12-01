6641587b4567cbd3623821aa7a48b7565e6101dc
Sunny (SunJae) Lee 이선재 <snjlee58@gmail.com>
2024-12-01 23:28:29 +0900

    Delete debug.sh

--- HW2/debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python3 cnt_kmer.py $1 $2 > genome1_kmers.txt
-python3 cnt_kmer.py $1 $3 > genome2_kmers.txt
-
-javac *.java
-java GenomeAlignment $1 genome1_kmers_test.txt genome2_kmers_test.txt $2 $3
