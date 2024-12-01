c0f1b02a25a4c532327c356df117a0fd7f0420cb
Sunny (SunJae) Lee 이선재 <snjlee58@gmail.com>
2024-12-01 23:28:20 +0900

    chore: cleaned up LCS code

--- HW2/LCS.java
+++ HW2/LCS.java
@@ -5,8 +5,6 @@ public class LCS {
     public static List<LCSKmer> findLCS(List<Kmer> genome1, List<Kmer> genome2) {
         int m = genome1.size();
         int n = genome2.size();
-        System.out.println("LCS array size=" + (long) m * n);
-        
         int[][] dp = new int[m + 1][n + 1];
 
         // Fill DP table
@@ -22,13 +20,10 @@ public class LCS {
 
         // Backtrack to find LCS
         List<LCSKmer> lcs = new ArrayList<>();
-        // List<Integer> genome1LCSPositions = new ArrayList<>();
-        // List<Integer> genome2LCSPositions = new ArrayList<>();
 
         int i = m, j = n;
         while (i > 0 && j > 0) {
             if (genome1.get(i - 1).value.equals(genome2.get(j - 1).value)) {
-                // lcs.add(genome1.get(i - 1).value); // Add to the front
                 
                 // Add the k-mer and its positions in both genomes to the result
                 lcs.add(new LCSKmer(
@@ -47,69 +42,7 @@ public class LCS {
 
         // Reverse the list because we added elements in reverse order during backtracking
         Collections.reverse(lcs);
-
+        System.out.println("LCS length=" + lcs.size()); // DEBUG
         return lcs;
     }
-
-    // public static List<Kmer> findLCS(List<Kmer> genome1, List<Kmer> genome2) {
-    //     int m = genome1.size();
-    //     int n = genome2.size();
-
-    //     // Only store two rows of the DP table
-    //     int[] prev = new int[n + 1];
-    //     int[] curr = new int[n + 1];
-
-    //     // Fill DP table using rolling arrays
-    //     for (int i = 1; i <= m; i++) {
-    //         for (int j = 1; j <= n; j++) {
-    //             if (genome1.get(i - 1).value.equals(genome2.get(j - 1).value)) {
-    //                 curr[j] = prev[j - 1] + 1;
-    //             } else {
-    //                 curr[j] = Math.max(prev[j], curr[j - 1]);
-    //             }
-    //         }
-    //         // Swap rows: current becomes previous
-    //         int[] temp = prev;
-    //         prev = curr;
-    //         curr = temp;
-    //     }
-
-    //     // Backtracking without full table reconstruction
-    //     List<Kmer> lcs = new ArrayList<>();
-    //     int i = m, j = n;
-
-    //     while (i > 0 && j > 0) {
-    //         // Recalculate only the relevant part of the current row
-    //         if (genome1.get(i - 1).value.equals(genome2.get(j - 1).value)) {
-    //             // Move diagonally: match found
-    //             lcs.add(0, genome1.get(i - 1)); // Add the matching Kmer to the front of the result
-    //             i--;
-    //             j--;
-    //         } else {
-    //             // Move based on the larger value between prev[j] and curr[j - 1]
-    //             if (prev[j] >= curr[j - 1]) {
-    //                 i--; // Move up
-    //             } else {
-    //                 j--; // Move left
-    //             }
-    //         }
-
-    //         // Swap arrays to "move up" in the rolling rows
-    //         int[] temp = curr;
-    //         curr = prev;
-    //         prev = temp;
-
-    //         // Recompute `curr` row values for the current `i` (backtracking context)
-    //         for (int k = 1; k <= n; k++) {
-    //             if (i > 0 && genome1.get(i - 1).value.equals(genome2.get(k - 1).value)) {
-    //                 curr[k] = prev[k - 1] + 1;
-    //             } else if (i > 0) {
-    //                 curr[k] = Math.max(prev[k], curr[k - 1]);
-    //             }
-    //         }
-    //     }
-
-    //     return lcs;
-    // }
-
 }
